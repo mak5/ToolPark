c63a3ce5c2018b4988d963e098bef1938919f7e4
Mohamed Ali KHOCHTALI <mkhochtali@mantu.com>
2020-05-11 23:59:48 +0100

    add dist files

---
+++ Presentation/ClientApp/dist/precache-manifest.47605c99f69fa0ecba00b2df6bc328d5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b96ea1f3aa4709f06bf",
+    "url": "/css/chunk-31b5ae4e.b4152702.css"
+  },
+  {
+    "revision": "58852e30ca077d12aead",
+    "url": "/css/chunk-66c3c8e6.0670aa22.css"
+  },
+  {
+    "revision": "2f646526e47b32b7109a",
+    "url": "/css/chunk-vendors.5b6ec3b0.css"
+  },
+  {
+    "revision": "63d2a5950fc212096c3612f71ae66f64",
+    "url": "/fonts/materialdesignicons-webfont.63d2a595.woff"
+  },
+  {
+    "revision": "7fb0e3780372f10f804513e2e0c39e78",
+    "url": "/fonts/materialdesignicons-webfont.7fb0e378.woff2"
+  },
+  {
+    "revision": "a1a0ed860f50ffd42ba46d7c10f30255",
+    "url": "/fonts/materialdesignicons-webfont.a1a0ed86.eot"
+  },
+  {
+    "revision": "fe1545ef4dd1eef2f1e25528898fc0b3",
+    "url": "/fonts/materialdesignicons-webfont.fe1545ef.ttf"
+  },
+  {
+    "revision": "e805ac00a5e99a288bd93dd14ad1e8f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a97b00b80940f7fcd4",
+    "url": "/js/app.0966124b.js"
+  },
+  {
+    "revision": "906e960fdfde8aa1bd38",
+    "url": "/js/chunk-2d0c51f3.fd607b75.js"
+  },
+  {
+    "revision": "8e9c6e3f30f60619737b",
+    "url": "/js/chunk-2d0d095f.936a5112.js"
+  },
+  {
+    "revision": "3b96ea1f3aa4709f06bf",
+    "url": "/js/chunk-31b5ae4e.1a1c1853.js"
+  },
+  {
+    "revision": "58852e30ca077d12aead",
+    "url": "/js/chunk-66c3c8e6.bd41d55b.js"
+  },
+  {
+    "revision": "2f646526e47b32b7109a",
+    "url": "/js/chunk-vendors.2db22f9e.js"
+  },
+  {
+    "revision": "ac862eb80974d56b5122e471ce456072",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
